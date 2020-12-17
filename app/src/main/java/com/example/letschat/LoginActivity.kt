e71683b83485b7e6b0eb179f8f116109c4d981ea
vickyy044 <43821289+vickyy044@users.noreply.github.com>
2020-12-17 13:49:28 +0530

    - Added firebase authentication  - new user signup added  - login added

--- app/src/main/java/com/example/letschat/LoginActivity.kt
+++ app/src/main/java/com/example/letschat/LoginActivity.kt
@@ -3,15 +3,25 @@ package com.example.letschat
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        val email = email_editText_login.text.toString()
+        val password = password_editText_login.text.toString()
+//        Firebase.auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+//            if(it.isSuccessful){
+//                Toast.makeText(this,"Login successful ",Toast.LENGTH_SHORT).show()
+//            }else Log.d("Login","Invalid details ${it.result}")
+//        }
     }
 }
 
--- app/src/main/java/com/example/letschat/MainActivity.kt
+++ app/src/main/java/com/example/letschat/MainActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,22 +13,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()        // remove actionBar from register activity
-        val email = email_editText_register.text.toString()
-        val password = password_editText_register.text.toString()
 
+        var email:String = email_editText_register.text.toString()
+        var password = password_editText_register.text.toString()
+        var name = username_editText_register.text.toString()
+        var registerButton = register_button_register
 
-        val registerButton = register_button_register
         registerButton.setOnClickListener {
             Log.d("MainActivity","Email is : $email")
             Log.d("MainActivity","Password is : $password")
+            Log.d("MainActivity","Username is: $name")
+            if(email.isEmpty() || password.isEmpty() || name.isEmpty()){
+                Toast.makeText(this,"enter valid details",Toast.LENGTH_SHORT).show()
+
+            }  // if email or password is empty just return clicklistener else app crashes
+            else{
+                performRegistration(email,password)
+            }
         }
 
-        FirebaseAuth.getInstance()
         already_have_account_textView_register.setOnClickListener {
             Log.d("MainActivity" , "Go to login activity")
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
-
         }
+    }
+
+    private  fun performRegistration(email:String,  password:String){
+        /* function to perform new user registration */
+
+        //Firebase authentication to create user with email and password
+        val auth = FirebaseAuth.getInstance()
+
+        auth?.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful)    return@addOnCompleteListener
+                    // else if authentication is successful
+                    else Log.d("Main","New user created with uid: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener {
+                    Log.d("Main","User registration failed : ${it.message}")
+                }
     }
 }
